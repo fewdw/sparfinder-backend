3a4d98dd19c60eba620a8c5d6661d19562a291b5
fewdw <frederic.alefebvre@gmail.com>
2024-09-07 16:43:50 -0400

    event entity

--- src/main/java/dev/sparfinder/fal/entity/Event.java
+++ src/main/java/dev/sparfinder/fal/entity/Event.java
@@ -1,4 +1,56 @@
 package dev.sparfinder.fal.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.Set;
+
+@Entity
+@Table(name = "events")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Event {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDate birthDate;
+
+    private LocalTime startTime;
+
+    private int duration;
+
+    private String name;
+
+    private String description;
+
+    private String location;
+
+    private boolean isPrivate;
+
+    @ManyToOne
+    @JoinColumn(name = "gym_id", nullable = false)
+    private Gym gym;
+
+    private int maxParticipants;
+
+    @ManyToMany(mappedBy = "participateList")
+    private Set<Boxer> participantList;
+
+    @ManyToMany(mappedBy = "waitingList")
+    private Set<Boxer> waitingList;
+
+    @ManyToMany(mappedBy = "inviteList")
+    private Set<Boxer> invitedList;
+
+
+
+
+    // add rating system :D
 }
